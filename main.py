cafa02be71fd9165db05fc8e17adf2c6c3e2f3cc
Eliseo Duque <duque.eliseo.7@gmail.com>
2025-11-15 11:27:00 -0600

    change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 
-dataFrame = pd.read_csv(r"/Users/edwinjaramillo/.cache/kagglehub/datasets/raspberrypie/oil-and-gas/versions/1/Oil and Gas 1932-2014.csv")
+dataFrame = pd.read_csv("Oil and Gas 1932-2014.csv")
 
 print(dataFrame)
